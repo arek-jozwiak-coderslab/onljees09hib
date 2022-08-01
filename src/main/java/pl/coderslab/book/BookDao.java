5396c8c5ca744b326ac42687e9409463d4364b9f
arek jozwiak <arkadiusz.jozwiak@coderslab.pl>
2022-08-01 16:48:00 +0200

    day 1

--- src/main/java/pl/coderslab/book/BookDao.java
+++ src/main/java/pl/coderslab/book/BookDao.java
@@ -6,7 +6,9 @@ import pl.coderslab.dog.Dog;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
@@ -14,6 +16,21 @@ public class BookDao {
 
     @PersistenceContext
     private EntityManager entityManager;
+
+
+    public List<Book> findAll(){
+        return entityManager
+                .createQuery("select b from Book b")
+                .getResultList();
+    }
+
+    public List<Book> findAllByRating(int rating){
+        return entityManager
+                .createQuery("select b from Book b where b.rating=:rat")
+                .setParameter("rat", rating)
+                .getResultList();
+    }
+
 
     public void save(Book book) {
         entityManager.persist(book);
